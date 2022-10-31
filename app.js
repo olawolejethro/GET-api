8ee2314fedcef73b22498c461c4dbba6e2b39a81
Olawole Jethro <kunlekzi2@gmail.com>
2022-10-31 16:01:22 +0100

    chore: Env variables

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
+  return res.status(200).json({
     slackUsername: "olawoleJethro",
     backend: true,
     age: 26,
-    bio: "My name is olawole jethro olakunle, am a software engineer and am a student of university of ilorin",
+    bio: "My name is olawole jethro olakunle, am a software engineer and am a student of University of Ilorin.",
   });
 });
 
-const PORT = 8080;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || "localhost";
+app.listen(PORT, HOST, () => {
   console.log(`app is listening to port ${PORT}`);
 });
